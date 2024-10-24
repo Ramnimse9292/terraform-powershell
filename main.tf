e07b3082b05b7a44a29cf7abcf0ce9a82942e14f
Ramnimse9292 <100694185+Ramnimse9292@users.noreply.github.com>
2024-10-24 16:30:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     powershell = {
       source  = "rfalias/powershell"
-      version = "~> 1.0"
+      version = "0.0.1"  # Specify the correct version
     }
   }
   required_version = ">= 0.12"
